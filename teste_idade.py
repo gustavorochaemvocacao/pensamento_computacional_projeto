d75f5492645846e35e4a8ebec3ddd1886f8ee654
gustavorochaemvocacao <gustavorocha.180909@gmail.com>
2026-03-09 16:32:00 -0300

    Add files via upload

---
+++ teste_idade.py
@@ -0,0 +1,20 @@
+'''
+
+Teste de idade
+
+
+'''
+print('digite sua idade')
+
+idade = int(input())
+
+if idade <= 18:
+    print('jovem é menor de idade') 
+
+elif idade >= 18  <= 67:
+    print('jovem é adulto de idade')
+
+else:
+    print('jovem é idoso de idade')
+
+
